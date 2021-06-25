f7f22f0e2e0771ce81e93d99d833d3180933fc7a
tomlk <longshenhao@vip.qq.com>
2021-06-25 08:26:02 +0800

    登录拦截器，一些前端展示。

--- springBoot/springboot03web/src/main/java/com/springboot03web/controller/LoginController.java
+++ springBoot/springboot03web/src/main/java/com/springboot03web/controller/LoginController.java
@@ -7,15 +7,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.thymeleaf.util.StringUtils;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class LoginController {
 
     @RequestMapping("/user/login")
     public String login(@RequestParam("username") String username,
                         @RequestParam("password") String password,
-                        Model model){
+                        Model model, HttpSession session){
         //具体的业务
         if(!StringUtils.isEmpty(username) && "123456".equals(password)){
+            session.setAttribute("loginUser",username);
             return "redirect:/main.html";
         }else{
             //告诉用户，用户名或者密码错误
