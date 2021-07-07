a00c2a039ce7e53ecc89f2eeb1c6e3a2cd10f7cb
tomlk <longshenhao@vip.qq.com>
2021-07-07 08:35:11 +0800

    springSecurity登录后权限管理

---
+++ springBoot/springboot-06-security/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+U
+%.mvn/wrapper/maven-wrapper.properties,3/2/32e074f71d16c1e88ac02aa83592ca6ad1fd98a8
+X
+(.mvn/wrapper/MavenWrapperDownloader.java,6/1/61ba2d75f3f04e099148e7a84c6a909f2000ff04
+€
+Psrc/test/java/com/springboot06security/Springboot06SecurityApplicationTests.java,9/d/9d0ab36db8ed3ac3accc26061cf858e7b56a256c
---
+++ springBoot/springboot-06-security/src/main/java/com/springboot06security/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.springboot06security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //授权
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //首页所有人可以访问，但是里面的功能页只有对应有权限的人才能访问
+        //请求授权的规则
+        http.authorizeRequests().
+                antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("vip1")
+                .antMatchers("/level2/**").hasRole("vip2")
+                .antMatchers("/level3/**").hasRole("vip3");
+
+        //没有权限默认会到登录页面，需要开启登录的页面
+        // /login
+        http.formLogin();
+    }
+
+    //认证
+    //密码编码：passwordEncoder
+    //在Spring Security 5.0中，新增了很多的加密方式。
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        //这些数据正常应该从数据库读
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("longkun").password(new BCryptPasswordEncoder().encode("123456")).roles("vip2","vip3")
+                .and()
+                .withUser("root").password(new BCryptPasswordEncoder().encode("123456")).roles("vip1","vip2","vip3")
+                .and()
+                .withUser("guest").password(new BCryptPasswordEncoder().encode("123456")).roles("vip1");
+    }
+}
